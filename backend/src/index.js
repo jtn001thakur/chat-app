c61102543f57fd390715d3afc298c692c33b0b10
jtn001thakur <jtn001thakur@gmail.com>
2025-08-18 11:06:28 +0530

    final commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,8 +20,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    // origin: "http://localhost:5173",
-    origin: "*",
+    origin: "http://localhost:5173",
+    // origin: "*",
     credentials: true,
   })
 );
